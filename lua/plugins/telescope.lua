d01c8245510e6c4884c57b0aa3bdc1ce3d7a1503
andreaspapadimitriou <ant_pap13@yahoo.gr>
2024-09-12 07:13:52 +0200

    add fzf remove telescope mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,25 +4,25 @@ return {
 		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
-			local builtin = require("telescope.builtin")
-
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-
-			local wk = require("which-key")
-			wk.add({
-				{ "<leader>f", group = "file" }, -- group
-				{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
-				{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep", mode = "n" },
-				{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers", mode = "n" },
-				{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags", mode = "n" },
-			})
-
+--			local builtin = require("telescope.builtin")
+--
+--			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+--			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+--			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+--			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+--
+--			local wk = require("which-key")
+--			wk.add({
+--				{ "<leader>f", group = "file" }, -- group
+--				{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
+--				{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep", mode = "n" },
+--				{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers", mode = "n" },
+--				{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags", mode = "n" },
+--			})
+--
 			local telescope = require("telescope")
 			telescope.setup({
-				pickers = {
+			pickers = {
 					find_files = {
 						hidden = true,
 					},
