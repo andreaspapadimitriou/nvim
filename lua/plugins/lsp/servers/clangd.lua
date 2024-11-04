4e1d67f856be7b58c1f24274a12a58a680e23cdb
Andreas Papadimitriou <ant_pap13@yahoo.gr>
2024-11-04 08:20:35 +0100

    chore: cleanup clangd lsp

--- lua/plugins/lsp/servers/clangd.lua
+++ lua/plugins/lsp/servers/clangd.lua
@@ -1,12 +1,8 @@
 local number_of_cores = 4
---if not _G.IS_WINDOWS or _G.IS_WSL then
---    number_of_cores = math.floor(tonumber(vim.fn.system("nproc")) * 0.5)
---end
-if _G.external then
-  print("External")
+if not _G.IS_WINDOWS or _G.IS_WSL then
+   number_of_cores = math.floor(tonumber(vim.fn.system("nproc")) * 0.5)
 end
---local paths = require('paths')
---print("Query Driver: " .. paths.query_driver)
+
 return {
     filetypes = {
         "c",
@@ -17,15 +13,13 @@ return {
     },
     cmd = {
         "clangd",
---        "-j=" .. tostring(number_of_cores),
+        "-j=" .. tostring(number_of_cores),
         "--background-index=true",
         "--clang-tidy",
         "--completion-style=detailed",
         "--malloc-trim",
         "--all-scopes-completion=true",
---        "--query-driver=" .. "/opt/sdks/**/*linux-g++",
-        "--query-driver=opt/sdks/rcsos-2.4.0/x86_4.4.50-rt63/sysroots/x86_64-rcssdk-linux/usr/bin/i586-rcs-linux/i586-rcs-linux-g++",
---        "--query-driver=" .. paths.query_driver,
+        "--query-driver=" .. "/opt/sdks/**/*linux-g++",
         "--header-insertion=iwyu",
     },
 }
