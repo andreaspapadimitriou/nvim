fdb40b818db5ea2086ffd8074f0c5c4cb60a4f93
andreaspapadimitriou <ant_pap13@yahoo.gr>
2025-04-20 20:46:29 +0200

    feat: add which-key for better keybinding discovery
    Adds which-key to improve discoverability of custom and default key
    mappings.

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -2,6 +2,7 @@
 require("lazy").setup({
     require("plugins.catppuccin"),
     require("plugins.treesitter"),
+    require("plugins.which_key"),
 }, {
     ui = {
         -- If you are using a Nerd Font: set icons to an empty table which will use the
