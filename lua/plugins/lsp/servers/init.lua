03c96c6cf204cfcfc1e599409ea9abc20afd4c88
Andreas Papadimitriou <ant_pap13@yahoo.gr>
2025-04-22 08:24:06 +0200

    feat: disable lsp_lines

--- lua/plugins/lsp/servers/init.lua
+++ lua/plugins/lsp/servers/init.lua
@@ -7,10 +7,10 @@ return {
             "*",
         },
     },
-    -- azure_pipelines_ls = require("plugins.lsp.servers.azure_pipelines_ls"),
     basedpyright = require("plugins.lsp.servers.basedPyright"),
     clangd = require("plugins.lsp.servers.clangd"),
-    -- cssls = require("plugins.lsp.servers.cssls"),
     lua_ls = require("plugins.lsp.servers.lua_ls"),
-    tsserver = require("plugins.lsp.servers.tsserver"),
+    -- azure_pipelines_ls = require("plugins.lsp.servers.azure_pipelines_ls"),
+    -- tsserver = require("plugins.lsp.servers.tsserver"),
+    -- cssls = require("plugins.lsp.servers.cssls"),
 }
