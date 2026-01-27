8d6e66b7a919dea8d79bef0d5d9820aaebfd327b
Andreas Papadimitriou <ant_pap13@yahoo.gr>
2026-01-27 08:12:35 +0100

    feat: update search replace keybind

--- lua/vim-remaps.lua
+++ lua/vim-remaps.lua
@@ -39,9 +39,9 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>sr", [[:%s/\V<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>sR", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("v", "<leader>sr", [[:s/\%V<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>]])
-
 -- Indent selected lines to the right in Visual mode
 vim.keymap.set("v", "<Tab>", ">gv", { noremap = true, silent = true })
 
