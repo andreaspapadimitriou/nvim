e40d6484cf8b19a79d269811ee18570d15cac76a
Andreas Papadimitriou <ant_pap13@yahoo.gr>
2026-01-30 17:35:50 +0100

    feat: vim-remaps update

--- lua/vim-remaps.lua
+++ lua/vim-remaps.lua
@@ -31,7 +31,6 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
---vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 -- navigate quickfix list and location list
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
@@ -48,18 +47,8 @@ vim.keymap.set("v", "<Tab>", ">gv", { noremap = true, silent = true })
 -- Indent selected lines to the left in Visual mode
 vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
 
---vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
---vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
-
---vim.keymap.set("n", "<leader><leader>", function()
---	vim.cmd("so")
---end)
-
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.api.nvim_command("source ~/.config/nvim/init.lua")
-    vim.api.nvim_command("source ~/.config/nvim/lua/vim-remaps.lua")
-    vim.api.nvim_command("source ~/.config/nvim/lua/vim-options.lua")
+    vim.cmd("so")
 end)
 
 -- Clear highlights on search when pressing <Esc> in normal mode
@@ -80,7 +69,7 @@ vim.keymap.set(
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+vim.keymap.set("t", "<Esc><esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- TIP: Disable arrow keys in normal mode
 vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
@@ -109,15 +98,14 @@ vim.api.nvim_set_keymap("i", "<C-S-v>", "<C-r>+", { noremap = true, silent = tru
 vim.api.nvim_set_keymap("n", "<C-q>", "<C-v>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<C-q>", "<C-v>", { noremap = true, silent = true })
 
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
+vim.keymap.set("n", "<leader>tw", ":set wrap!<CR>", { noremap = true, silent = true })
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    group = vim.api.nvim_create_augroup("kickstat-highlight-yank", { clear = true }),
     callback = function()
         vim.highlight.on_yank()
     end,
