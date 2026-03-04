0f9bd22868d3bbf574725d091fcd74219143a71f
Andreas Papadimitriou <ant_pap13@yahoo.gr>
2026-03-04 09:42:25 +0100

    fix: command line popup dynamic recentering

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,39 +1,4 @@
 return {
-    -- Better `vim.notify()`
-    -- {
-    --     "rcarriga/nvim-notify",
-    --     keys = {
-    --         {
-    --             "<leader>un",
-    --             function()
-    --                 require("notify").dismiss({ silent = true, pending = true })
-    --             end,
-    --             desc = "Dismiss All Notifications",
-    --         },
-    --     },
-    --     opts = {
-    --         stages = "static",
-    --         timeout = 6000,
-    --         max_height = function()
-    --             return math.floor(vim.o.lines * 0.75)
-    --         end,
-    --         max_width = function()
-    --             return math.floor(vim.o.columns * 0.75)
-    --         end,
-    --         on_open = function(win)
-    --             vim.api.nvim_win_set_config(win, { zindex = 100 })
-    --         end,
-    --     },
-    --     init = function()
-    --         -- when noice is not enabled, install notify on VeryLazy
-    --         --    if not LazyVim.has 'noice.nvim' then
-    --         --    LazyVim.on_very_lazy(function()
-    --         vim.notify = require("notify")
-    --         --  end)
-    --         --end
-    --     end,
-    -- },
-    -- Cmdline and search UI
     {
         "folke/noice.nvim",
         event = "VeryLazy",
@@ -65,7 +30,7 @@ return {
                 cmdline_popup = {
                     position = {
                         row = 10, -- distance from top
-                        col = math.floor(vim.o.columns / 2) - 60 / 2, -- distance from left
+                        col = "50%", -- dynamically centers on resize
                     },
                     size = {
                         width = 60,
