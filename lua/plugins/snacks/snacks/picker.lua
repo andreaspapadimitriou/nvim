0eddbef32310f842aee6a644a321c4e828d6640d
Andreas Papadimitriou <ant_pap13@yahoo.gr>
2026-03-05 08:16:16 +0100

    feat: add picker search options
    close buffer, toggle regex and fuzzy search keybinds

--- lua/plugins/snacks/snacks/picker.lua
+++ lua/plugins/snacks/snacks/picker.lua
@@ -12,5 +12,44 @@ return function(opts)
                 truncate = 40,
             },
         },
+        sources = {
+            buffers = {
+                title = "Buffers (<ctrl-d> to close)",
+                win = {
+                    input = {
+                        keys = {
+                            ["<c-d>"] = { "bufdelete", mode = { "n", "i" } },
+                        },
+                    },
+                },
+                actions = {
+                    bufdelete = function(picker, item)
+                        if item then
+                            vim.api.nvim_buf_delete(item.buf, { force = false })
+                            picker:find()
+                        end
+                    end,
+                },
+            },
+            grep = {
+                title = "Grep (<ctrl-g> to Regex)",
+                win = {
+                    input = {
+                        keys = {
+                            ["<c-g>"] = { "toggle_regex", mode = { "n", "i" } },
+                        },
+                    },
+                },
+                actions = {
+                    toggle_regex = function(picker)
+                        picker.opts.regex = not picker.opts.regex
+                        local title = picker.opts.regex and "Grep (<ctrl-g> to Fuzzy)"
+                            or "Grep (<ctrl-g> to Regex)"
+                        picker:set_title(title)
+                        picker:find()
+                    end,
+                },
+            },
+        },
     }
 end
